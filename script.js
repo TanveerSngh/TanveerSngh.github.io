ab9f2ddcf5d11d8384f162fb9c31de5dc8a60a19
TanveerSngh <tanveer.singh5764@gmail.com>
2025-01-26 00:44:17 -0500

    first version

---
+++ script.js
@@ -0,0 +1,40 @@
+0; // 
+1334151855;
+12341.1515;
+Infinity;
+-13434;
+-Infinity;
+-0;
+
+var kitty;
+
+kitty = 5
+
+var kitty = 24;
+
+console.log(Kitty);
+
+kitty = kitty;
+
+console.log(kitty);
+
+var doggy;
+console.log(doggy);
+
+var prose = 'hello';
+var moreprose ="hello x2";
+
+var x =true;
+x = false;
+
+var ball = {};
+ball = null;
+
+5 + 5;
+kitty = 5 + 5;
+var kitty;
+
+kitty = 5 + "5";
+console.log(kitty);
+
+console.log(kitty + "hello");
---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+    createCanvas(720, 400);
+  }
+  
+  function draw() {
+    background(102);
+  
+    push();
+    translate(width * 0.5, height * 0.5);
+    rotate(frameCount / 50.0);
+    star(200, 210, 1000, 200, 200);
+    pop();
+    
+  }
+  
+  
+  function star(x, y, radius1, radius2, npoints) {
+    let angle = TWO_PI / npoints;
+    let halfAngle = angle / 2.0;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius2;
+      let sy = y + sin(a) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a + halfAngle) * radius1;
+      sy = y + sin(a + halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+  
